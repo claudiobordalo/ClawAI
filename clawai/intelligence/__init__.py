14156303d80987bfc2da959fd59a12a7aa25ea3b
ClawDio <claudio.fgcosta@gmail.com>
2026-06-29 20:32:27 -0300

    fix: keep intelligence package import lightweight

--- clawai/intelligence/__init__.py
+++ clawai/intelligence/__init__.py
@@ -1,13 +1,7 @@
-from .broker import IntelligenceAnalysis, IntelligenceOrchestrator, ToolBroker, cognition_broker, intelligence_orchestrator
 from .memory import SemanticMemoryEntry, SemanticMemoryStore, semantic_memory
 
 __all__ = [
-    "IntelligenceAnalysis",
-    "IntelligenceOrchestrator",
     "SemanticMemoryEntry",
     "SemanticMemoryStore",
-    "ToolBroker",
-    "cognition_broker",
-    "intelligence_orchestrator",
     "semantic_memory",
 ]
