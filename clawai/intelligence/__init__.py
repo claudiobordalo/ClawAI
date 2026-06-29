1bc980e86148fc5c530bb859049b210c1b2eb8ce
ClawDio <claudio.fgcosta@gmail.com>
2026-06-29 20:28:22 -0300

    feat: add intelligence package exports

---
+++ clawai/intelligence/__init__.py
@@ -0,0 +1,12 @@
+from .broker import IntelligenceAnalysis, IntelligenceOrchestrator, ToolBroker, tool_broker
+from .memory import SemanticMemoryEntry, SemanticMemoryStore, semantic_memory
+
+__all__ = [
+    "IntelligenceAnalysis",
+    "IntelligenceOrchestrator",
+    "SemanticMemoryEntry",
+    "SemanticMemoryStore",
+    "ToolBroker",
+    "semantic_memory",
+    "tool_broker",
+]
