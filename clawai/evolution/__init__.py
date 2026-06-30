75c7e5d2904a3eb9c43e90aab67585066577511a
ClawDio <claudio.fgcosta@gmail.com>
2026-06-29 22:20:12 -0300

    feat: add evolution engine package exports

---
+++ clawai/evolution/__init__.py
@@ -0,0 +1,17 @@
+from .engine import (
+    EvolutionAnalysis,
+    EvolutionBacklogItem,
+    EvolutionCycleRecord,
+    EvolutionEngine,
+    EvolutionState,
+    evolution_engine,
+)
+
+__all__ = [
+    "EvolutionAnalysis",
+    "EvolutionBacklogItem",
+    "EvolutionCycleRecord",
+    "EvolutionEngine",
+    "EvolutionState",
+    "evolution_engine",
+]
