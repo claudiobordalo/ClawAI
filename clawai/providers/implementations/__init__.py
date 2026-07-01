bed8558ed8689779ccc6f606732450859e713669
ClawDio <claudio.fgcosta@gmail.com>
2026-07-01 16:56:05 -0300

    fix: import only installed providers at startup

--- clawai/providers/implementations/__init__.py
+++ clawai/providers/implementations/__init__.py
@@ -1,11 +1,7 @@
-﻿from .anthropic_provider import AnthropicProvider
-from .google_provider import GoogleProvider
 from .ollama_provider import OllamaProvider
 from .openai_provider import OpenAIProvider
 
 __all__ = [
-    "AnthropicProvider",
-    "GoogleProvider",
     "OllamaProvider",
     "OpenAIProvider",
 ]
