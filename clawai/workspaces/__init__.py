148d36de326f55e62eabe44a53004f71884a4ace
ClawDio <claudio.fgcosta@gmail.com>
2026-07-02 21:28:08 -0300

    Export project analyzer from workspace package

--- clawai/workspaces/__init__.py
+++ clawai/workspaces/__init__.py
@@ -1,9 +1,12 @@
 # clawai/workspaces/__init__.py
+from .analyzer import ProjectAnalyzer, ProjectMap
 from .manager import WorkspaceInfo, WorkspaceManager, workspace_manager
 
 workspace_state = workspace_manager
 
 __all__ = [
+    "ProjectAnalyzer",
+    "ProjectMap",
     "WorkspaceInfo",
     "WorkspaceManager",
     "workspace_manager",
