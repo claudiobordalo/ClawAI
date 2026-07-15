b75c36604fd53dd44b5ab4cf7fce35d2923ea70a
ClawDio <claudio.fgcosta@gmail.com>
2026-07-15 18:51:13 -0300

    Export LM Studio provider implementation

--- clawai/providers/implementations/__init__.py
+++ clawai/providers/implementations/__init__.py
@@ -1,7 +1,9 @@
+from .lmstudio_provider import LMStudioProvider
 from .ollama_provider import OllamaProvider
 from .openai_provider import OpenAIProvider
 
 __all__ = [
+    "LMStudioProvider",
     "OllamaProvider",
     "OpenAIProvider",
 ]
